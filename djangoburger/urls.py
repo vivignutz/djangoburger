88ee3edf20473ae6e6676b9cb64159cee6d841b1
vivignutz <vgnutzmann@gmail.com>
2022-12-26 11:50:43 +0000

    changed name of app to avoid confusion and restarted the code

--- djangoburger/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core.views import home
-
-urlpatterns = [
-    path('admin/', admin, site, urls),
-    path('home/', home, name='home')
-]
--- mydjangoburger/views.py
+++ mydjangoburger/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
