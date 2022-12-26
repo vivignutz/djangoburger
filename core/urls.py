5aa87ccf502141ff5f214a5fdd7491de042d388e
vivignutz <vgnutzmann@gmail.com>
2022-12-26 12:30:47 +0000

    changed files name as well

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mydjangoburguer.urls'))
+    path('', include('djangoburguer.urls'))
 ]
