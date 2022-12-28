7bd06635da61f9fabea86f372341bd06687567d7
vivignutz <vgnutzmann@gmail.com>
2022-12-28 13:51:40 +0000

    Add model and super admin

--- djangoburger/models.py
+++ djangoburger/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    description: models.TextField(null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
+    
+    def __str__(self):
+        return_self.description
