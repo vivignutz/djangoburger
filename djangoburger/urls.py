d4557428a0cb1c5a3130caa2e9665da31e7d2e12
vivignutz <vgnutzmann@gmail.com>
2022-12-30 12:21:33 +0000

    Add links at navbar

--- djangoburger/urls.py
+++ djangoburger/urls.py
@@ -5,8 +5,18 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import (home,
+                    products,
+                    contact,
+                    login,
+                    register
+)
+
 urlpatterns = [
     path('djangoburger/', views.home, name='home'),
     path('product/', views.details_product, name='product')
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
