9c900233160502b16f3279a385710dadc947dba7
vivignutz <vgnutzmann@gmail.com>
2022-12-30 13:18:33 +0000

    add navigation links to navbar

--- djangoburger/views.py
+++ djangoburger/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here:
 
 
 def home(request):
-    return HttpResponse('Hello Django!')
+    return render(request, 'djangoburger/home.html', {})
 
 
 def details_product(request):
